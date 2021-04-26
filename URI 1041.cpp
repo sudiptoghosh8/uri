716514a897729404498e2b4738534b8a7f45c473
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-04-26 23:43:26 +0600

    Add files via upload

---
+++ URI 1041.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b;
+    while(cin>>a>>b)
+    {
+        if(a==0&&b==0)
+        {
+            cout<<"Origem"<<endl;
+        }
+        else if(a>0&&b>0)
+        {
+            cout<<"Q1"<<endl;
+        }
+        else if(a<0&&b>0)
+        {
+            cout<<"Q2"<<endl;
+        }
+        else if(a<0&&b<0)
+        {
+            cout<<"Q3"<<endl;
+        }
+        else if(a>0&&b<0)
+        {
+            cout<<"Q4"<<endl;
+        }
+        else if(b==0)
+        {
+            cout<<"Eixo X"<<endl;
+        }
+        else
+        {
+            cout<<"Eixo Y"<<endl;
+        }
+    }
+    return 0;
+}
