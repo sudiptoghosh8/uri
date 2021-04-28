953f5e88ed8d6ede47afb33af78e68ecb813ca3c
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-04-28 17:31:18 +0600

    Add files via upload

---
+++ URI 1070 Six Odd Numbers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,i;
+
+    i=12;
+    cin>>n;
+    while(i--)
+    {
+        if(n%2!=0)
+        {
+            cout<<n<<endl;
+        }
+    n++;
+    }
+
+    return 0;
+}
