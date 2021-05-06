07cc6e6f7f0648b27abe628abf5bdde15e8620cc
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-05-06 21:19:19 +0600

    Add files via upload

---
+++ URI 1095 Sequence IJ 1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    j=60;
+    for(i=1;i<=39;i+=3)
+    {
+        cout<<"I="<<i<<" J="<<j<<endl;
+       cout<<endl;
+        j-=5;
+
+    }
+    return 0;
+}
