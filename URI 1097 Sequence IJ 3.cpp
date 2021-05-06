6e0187a0dff11644d734fd0ed927a3e03c09853f
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-05-06 21:39:32 +0600

    Add files via upload

---
+++ URI 1097 Sequence IJ 3.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+   cout<<"I=1 "<<"J=7"<<endl;
+   cout<<"I=1 "<<"J=6"<<endl;
+   cout<<"I=1 "<<"J=5"<<endl;
+
+   cout<<"I=3 "<<"J=9"<<endl;
+   cout<<"I=3 "<<"J=8"<<endl;
+   cout<<"I=3 "<<"J=7"<<endl;
+
+   cout<<"I=5 "<<"J=11"<<endl;
+   cout<<"I=5 "<<"J=10"<<endl;
+   cout<<"I=5 "<<"J=9"<<endl;
+
+   cout<<"I=7 "<<"J=13"<<endl;
+   cout<<"I=7 "<<"J=12"<<endl;
+   cout<<"I=7 "<<"J=11"<<endl;
+
+   cout<<"I=9 "<<"J=15"<<endl;
+   cout<<"I=9 "<<"J=14"<<endl;
+   cout<<"I=9 "<<"J=13"<<endl;
+
+    return 0;
+}
